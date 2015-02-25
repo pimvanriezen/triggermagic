f75c0cdc042bbff8e76df673b37c05c0944ec939
Pim van Riezen <pi@madscience.nl>
2015-02-25 12:15:15 +0100

    Define the button event thread and api

---
+++ btevent.h
@@ -0,0 +1,61 @@
+#ifndef _BTEVENT_H
+#define _BTEVENT_H 1
+
+/* =============================== TYPES =============================== */
+
+#define BTID_UP         0
+#define BTID_DOWN       1
+#define BTID_MINUS      2
+#define BTID_PLUS       3
+#define BTID_SHIFT      4
+#define BTID_LEFT       5
+#define BTID_RIGHT      6
+#define BTID_CLICK      7
+
+#define BTMASK_UP       (1 << BTID_UP)
+#define BTMASK_DOWN     (1 << BTID_DOWN)
+#define BTMASK_MINUS    (1 << BTID_MINUS)
+#define BTMASK_PLUS     (1 << BTID_PLUS)
+#define BTMASK_SHIFT    (1 << BTID_SHIFT)
+#define BTMASK_LEFT     (1 << BTID_LEFT)
+#define BTMASK_RIGHT    (1 << BTID_RIGHT)
+#define BTMASK_CLICK    (1 << BTID_CLICK)
+
+/** Event flowing out of the button manager. Represents a specific
+    configuration of buttons pressed. */
+typedef struct button_event_s {
+    struct button_event_s   *next; /**< List-link for the queue */
+    uint8_t                  buttons; /**< Button state */
+    bool                     isrepeat; /**< true if it's a repetition */
+} button_event;
+
+/** Represents the state of a single button */
+typedef struct button_state_s {
+    bool        pressed; /**< true if button is pressed */
+    uint64_t    lastchange; /**< tick number of last state change */
+} button_state;
+
+/** Class object for the button manager */
+typedef struct button_manager_s {
+    thread           super; /**< The thread this all runs on */
+    button_state     states[8]; /**< State of buttons during this tick */
+    uint64_t         tick; /**< Current tick number */
+    conditional      eventcond; /**< Conditional for new events */
+    button_event    *first; /**< Linked-list head */
+    button_event    *last; /**< Linked-list tail */
+    bool             useshift; /**< True if we should treat shift as shift */
+    pthread_mutex_t  lock; /**< Mutex for the event queue */
+} button_manager;
+
+/* ============================== GLOBALS ============================== */
+
+extern button_manager *BT;
+
+/* ============================= FUNCTIONS ============================= */
+
+void             button_manager_init (void);
+void             button_manager_main (thread *);
+void             button_manager_add_event (uint8_t, bool);
+button_event    *button_manager_wait_event (bool useshift);
+
+void             button_event_free (button_event *);
