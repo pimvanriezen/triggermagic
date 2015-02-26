bac43d4561ed36e2784349a63a1e0aad8bf75a9d
Pim van Riezen <pi@madscience.nl>
2015-02-26 22:31:45 +0100

    load/save presets to sd. Initialize empty presets on load.

--- main.c
+++ main.c
@@ -21,17 +21,36 @@ void context_init (void) {
     CTX.presets[1].triggers[7].notes[0] = 62;
     CTX.presets[1].triggers[8].notes[0] = 63;
     context_load_preset (1);
+    FILE *pst = fopen ("/home/pi/triggermagic.presets");
+    if (pst) {
+        fread (CTX.presets, sizeof(preset), 100, pst);
+        fclose (pst);
+    }
 }
 
 void context_load_preset (int nr) {
     if (nr<1 || nr>99) return;
     memcpy (&CTX.preset, CTX.presets+nr, sizeof (preset));
     CTX.preset_nr = nr;
+    if (CTX.preset.name[0] == 0) {
+        strcpy (CTX.preset.name, "Init");
+        CTX.preset.tempo = 125;
+        for (int i=0; i<12; ++i) {
+            CTX.preset.triggers[i].move - MOVE_LOOP_UP;
+            CTX.preset.triggers[i].sgate = 50;
+            CTX.preset.triggers[i].seqlen = 8;
+        }
+    }
 }
 
 void context_store_preset (void) {
     if (CTX.preset_nr < 1 || CTX.preset_nr > 99) return;
     memcpy (CTX.presets + CTX.preset_nr, &CTX.preset, sizeof (preset));
+    FILE *pst = fopen ("/home/pi/triggermagic.presets.new","w");
+    size_t res = fwrite (CTX.presets, sizeof(preset), 100, pst);
+    fclose (pst);
+    if (res == 100) rename ("/home/pi/triggermagic.presets.new",
+                            "/home/pi/triggermagic.presets");
 }
 
 int main (int argc, const char *argv[]) {
