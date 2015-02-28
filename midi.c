f2cc3ccfe3026c49e48a145b1401d4b79fe76366
Pim van Riezen <pi@madscience.nl>
2015-02-28 03:51:34 +0100

    underscoreo

--- midi.c
+++ midi.c
@@ -315,7 +315,7 @@ void midi_receive_thread (thread *t) {
 
 void midi_send_thread (thread *t) {
     while (1) {
-        uint64_t qnote = 60000 / CTX.preset_tempo;
+        uint64_t qnote = 60000 / CTX.preset.tempo;
         uint64_t now = getclock();
         int c = self.current;
         if (c>=0) {
